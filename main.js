651cf93ed4f6471f63eb5a9bb35fe48b1e9cae01
Tom <TheOnly-Tom@users.noreply.github.com>
2017-06-22 19:16:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,39 @@ main.js
 GameJs 
 https://github.com/TheOnly-Tom/gamejs/
 */
+var VERBOOSE = true;
+
+var gamePlayer = {
+  create: function(parentNode, height, width, tick) {
+    this.parent = parentNode;
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.height = height;
+    this.width = width;
+    this.tick = tick;
+    this.running = 0;
+    
+    this.canvas.height = this.height;
+    this.canvas.width = this.width;
+    this.parent.appendChild(this.canvas);
+    if (VERBOOSE) {
+      console.log("gamePlayer created.",this.parent,this.canvas,this.ctx,this);
+    }
+  },
+  loop: function() {
+    this.ctx.clearRect(0,0,this.width,this.height);
+  },
+  start: function() {
+    this.running = setInterval(this.loop,this.tick);
+    if (VERBOOSE) {
+      console.log("gamePlayer started.",this);
+    }
+  },
+  end: function() {
+    clearInterval(this.running);
+    this.running = 0;
+};
+
+var gameObject = {
+ 
+};
